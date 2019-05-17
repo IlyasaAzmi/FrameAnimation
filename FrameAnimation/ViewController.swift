f4d62b45eb86c174f833404b4080acad68c50584
Ilyasa Azmi <theazmi99ilyasa@gmail.com>
2019-05-17 08:57:34 +0700

    Initial Commit

---
+++ FrameAnimation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FrameAnimation
+//
+//  Created by Ilyasa Azmi on 17/05/19.
+//  Copyright © 2019 Ilyasa Azmi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
